0e3197a4c575e2a49d6a01f4280a6e8813968328
Лаисат <lz_256@mail.ru>
2024-04-11 22:54:04 +0300

    login

---
+++ src/main/java/com/example/exampie/ExampieApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exampie/ExampieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
